6d7e59cce2fecea19fe4b0c6412e5a9ef1532110
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-20 17:44:48 -0300

    Requisição com o método POST

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,12 +1,26 @@
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
     const conexaoConvertida = await conexao.json();
-    
+
     return conexaoConvertida;
 }
 
-listaVideos();
+async function criaVideo() {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content=type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+}
 
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
